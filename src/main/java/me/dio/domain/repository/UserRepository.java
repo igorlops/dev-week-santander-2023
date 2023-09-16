3fc79d4c0eaf6454e3e64267566dec34340806cd
Igor Lopes <claudioigorai@gmail.com>
2023-09-15 22:30:32 -0300

    Finalizando cama de negocio

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import me.dio.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	boolean existsByAccountNumber(String accoutnNumber);
 	
 }
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	private final UserRepository repository;
+	
+	public UserServiceImpl(UserRepository repository) {
+		this.repository = repository;
+	}
+	@Override
+	public User findById(Long id) {
+		return repository.findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	public User create(User userToCreate) {
+		if(userToCreate.getId() != null && repository.existsById(userToCreate.getId())) {
+		
+		throw new IllegalArgumentException("This User ID already exists");
+	}
+		if(repository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+			throw new IllegalArgumentException("This account number already exists");
+		}
+		return repository.save(userToCreate);
+	}
+
+}
